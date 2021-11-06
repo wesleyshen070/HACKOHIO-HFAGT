97dbc73df16e6555f6f4cd4a22ca29d2add2f10f
Wesley Shen <wesleyshen070@gmail.com>
2021-11-06 17:36:12 -0400

    separate js and html files

--- app.js
+++ app.js
@@ -1,16 +1,59 @@
-'use strict'
+const xlabels = [];
+const yVals = [];
 
-const switcher = document.querySelector('.btn');
+chartIt();
 
-switcher.addEventListener('click', function () {
-    document.body.classList.toggle('dark-theme')
-
-    var className = document.body.className;
-    if (className == "light-theme") {
-        this.textContent = "Dark";
-    }
-    else {
-        this.textContent = "Light";
-    }
-
-});
+async function chartIt() {
+    await getData();
+    const ctx = document.getElementById('chart').getContext('2d');
+    const myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: xlabels,
+            datasets: [{
+                label: 'Prescriber\'s Aggregated TRx',
+                data: yVals,
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+}
+async function getData() {
+    const response = await fetch('Prescriber_Data.csv');
+    const data = await response.text();
+    
+    const table = data.split('\r\n').slice(1);
+    
+    var shorten = 0;
+    
+    table.forEach (row =>{  
+        shorten++;
+        if(shorten >= 99)
+        return;
+        
+        const columns = row.split(',');
+        const doctor = columns[1] + " " + columns[2];
+        xlabels.push(doctor);
+        
+        RTxTotal = 0;
+        for(let i = 11; i < 17; i++){
+            RTxTotal += parseInt(columns[i]);
+        }
+        
+        yVals.push(RTxTotal);
+        console.log(doctor, RTxTotal);
+    });
+}
