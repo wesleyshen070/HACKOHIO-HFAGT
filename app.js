b71ecf4deb694e407c410468298f2b6bd3cb3bbf
Wesley Shen <wesleyshen070@gmail.com>
2021-11-06 20:36:42 -0400

    override to correct

--- app.js
+++ app.js
@@ -1,16 +1,97 @@
-'use strict'
+const xlabels = [];
+const yTRxVals = [];
+const yNRxVals = [];
+var myChart;
+var drugName = [];
+var numPrescribers = 0;
+var defaultDisplay = 50;
+var currentIndex = defaultDisplay;
 
-const switcher = document.querySelector('.btn');
+chartIt(defaultDisplay);
 
-switcher.addEventListener('click', function () {
-    document.body.classList.toggle('dark-theme')
+async function chartIt(numDisplay) {
+    await getData(numDisplay);
+    
+    const ctx = document.getElementById('chart').getContext('2d');
+    myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: xlabels,
+            datasets: [
+                {
+                    label: 'Prescriber\'s Aggregated TRx',
+                    data: yTRxVals,
+                    backgroundColor: 'rgba(255, 99, 132, 0.2)',
+                    borderColor: 'rgba(255, 99, 132, 1)' ,
+                    borderWidth: 1
+                },
+                {
+                    label: 'Prescriber\'s Aggregated NRx',
+                    data: yNRxVals,
+                    backgroundColor: 'rgba(0, 99, 132, 0.2)',
+                    borderColor: 'rgba(0, 99, 132, 1)' ,
+                    borderWidth: 1
+                }
+            ]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+}
 
-    var className = document.body.className;
-    if (className == "light-theme") {
-        this.textContent = "Dark";
-    }
-    else {
-        this.textContent = "Light";
-    }
+async function getData(numDisplay) {
+    const response = await fetch('Prescriber_Data.csv');
+    const data = await response.text();
+    
+    const table = data.split('\r\n').slice(1);
+    
+    // # of different drugs at the moment
+    var iterations = 0;
+    
+    table.forEach (row =>{  
+        // comment in to artifically shorten
+        iterations++;
+        if(iterations >= numDisplay)
+            return;
 
-});
+        const columns = row.split(',');
+        const doctor = columns[1] + " " + columns[2];
+        xlabels.push(doctor);
+
+        if(columns[1]=='undefined')
+            return;
+        
+        NRxTotal = 0;
+        TRxTotal = 0;
+        for(let i = 5; i < 11; i++){
+            NRxTotal += parseInt(columns[i]);
+        }
+        for(let i = 11; i < 17; i++){
+            TRxTotal += parseInt(columns[i]);
+        }
+        
+        //for the creation of 4 different charts
+        drugName.push(columns[4]);
+        var uniqueDrugs = new Set(drugName);
+        drugName = Array.from(uniqueDrugs);
+        console.log(drugName);
+        
+        yNRxVals.push(NRxTotal);
+        yTRxVals.push(TRxTotal);
+    });
+}
+
+function getInputValue(){
+    // Selecting the input element and get its value 
+    var inputVal = document.getElementById("myInput").value;
+    
+    // Displaying the value
+    myChart.clear();
+    chartIt(inputVal);
+    myChart.update();
+}
